5714990553d6a96edbf21a4706f4b90f4e78bf75
PraydE007 <prayde.niste@gmail.com>
2020-04-01 18:45:45 +0300

    Start Again Again Again

---
+++ TheFinderRobot/Assets/Scripts/TouchOne1.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchOne1 : MonoBehaviour
+{
+    public GameObject inputField;
+    public string Direct;
+    public string Color;
+
+
+    void OnMouseUp()
+    {
+       if (Input.GetMouseButtonUp(0)) {
+            inputField.GetComponent<SetActive>().touch = true;
+            inputField.GetComponent<SetActive>().button = 0;
+            Debug.Log(gameObject.tag);
+
+       }
+    }
+}
---
+++ TheFinderRobot/Assets/Scripts/Touchtwo.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Touchtwo : MonoBehaviour
+{
+    public GameObject inputField;
+    
+    public string Direct;
+    public string Color;
+
+    void OnMouseUp() {
+         if (Input.GetMouseButtonUp(0)) {
+            inputField.GetComponent<SetActive>().touch = true;
+            inputField.GetComponent<SetActive>().button = 1;
+             //    Debug.Log(gameObject.tag);
+        }
+    }
+}
