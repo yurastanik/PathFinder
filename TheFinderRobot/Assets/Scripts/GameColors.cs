63e71c516d10216eb084b83c3582be55002cf7fa
PraydE007 <prayde.niste@gmail.com>
2020-04-08 15:42:27 +0300

    MapLoader

---
+++ TheFinderRobot/Assets/Scripts/GameColors.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameColors {
+
+    public static Color GetColor(int col) {
+        if (col == 1)
+            return Color.green;
+        else if (col == 2)
+            return Color.red;
+        return Color.black;
+    }
+
+}
---
+++ TheFinderRobot/Assets/Scripts/MapLoader.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class MapLoader : MonoBehaviour {
+
+    [SerializeField] private string mapName;
+    [SerializeField] private GameObject columnPrefab;
+
+    private StreamReader stream;
+    private string path;
+    private Map loadedMap;
+
+    private void Start() {
+        path = Application.dataPath + "/Maps/" + mapName + ".json";
+        stream = new StreamReader(path);
+        loadedMap = JsonUtility.FromJson<Map>(stream.ReadToEnd());
+        RenderMap(loadedMap.map, loadedMap.mapWidth);
+    }
+
+    public void OnMapUpdate(int[,] newMap) {
+        loadedMap.map = TwoDToOneDArray(newMap);
+        RenderMap(loadedMap.map, loadedMap.mapWidth);
+    }
+
+    private void RenderMap(int[] map, int width) {
+        int rows = map.Length / width;
+        int col = 0;
+
+        foreach (GameObject child in transform)
+            GameObject.Destroy(child);
+        for (int y = 0; y < rows; y++) {
+            for (int x = 0; x < width; x++) {
+                col = map[(y * width) + x];
+                if (col != 0)
+                    CreateColumn(new Vector3Int(x * 2, 0, y * 2), col);
+            }
+        }
+    }
+
+    private void CreateColumn(Vector3Int pos, int col) {
+        GameObject newChild = GameObject.Instantiate(columnPrefab, transform);
+        MeshRenderer topRend;
+
+        newChild.transform.position = pos;
+        Transform[] childs = newChild.GetComponentsInChildren<Transform>();
+        for (int i = 0; i < childs.Length; i++) {
+            if (childs[i].name == "Top") {
+                topRend = childs[i].GetComponent<MeshRenderer>();
+                topRend.material.color = GameColors.GetColor(col);
+                break;
+            }
+        }
+    }
+
+    public int[,] OneDToTwoDArray(int[] map, int width) {
+        int rows = map.Length / width;
+        int[,] res = new int[width, rows];
+
+        for (int y = 0; y < rows; y++) {
+            for (int x = 0; x < width; x++) {
+                res[y, x] = map[(y * width) + x];
+            }
+        }
+        return res;
+    }
+
+    public int[] TwoDToOneDArray(int[,] map) {
+        int[] res = new int[map.Length];
+        int i = 0;
+
+        for (int y = 0; y < map.GetLength(0); y++) {
+            for (int x = 0; x < map.GetLength(1); x++) {
+                res[i] = map[y, x];
+                i++;
+            }
+        }
+        return res;
+    }
+
+}
+
+public class Map {
+    public int[] map;
+    public int mapWidth;
+    public int[] moves;
+    public Vector2Int startPos;
+    public string direction;
+}
