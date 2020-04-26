f2fd8cb212b2f3bf41ab59a46fef8b5df99ee906
Oleksandr Stanislavskyy <36114963+Zemerr@users.noreply.github.com>
2020-04-26 20:27:50 +0300

    finally refresh

---
+++ TheFinderRobot/Assets/Scripts/Rainbow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rainbow : MonoBehaviour {
+
+    private MeshRenderer meshRenderer;
+
+    private float speed = 0.005f;
+    private float HSV;
+
+    private void Start() {
+        int x = (int)transform.position.x;
+
+        meshRenderer = GetComponent<MeshRenderer>();
+        HSV = (x / 2) * 0.1f;
+    }
+
+    private void Update() {
+        meshRenderer.material.color = Color.HSVToRGB(HSV, 1, 1);
+
+        HSV -= speed;
+        if (HSV < 0)
+            HSV = 1;
+    }
+}
